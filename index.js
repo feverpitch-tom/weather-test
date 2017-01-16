14be0f5e944bfd3287858d273dda117f47018a02
Tom Groves <T.Groves@amaze.com>
2017-01-16 13:30:48 +0000

    Added route for retrieval of forecast by co-ords

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+// koa
 const koa = require('koa')
 const app = koa()
 const _ = require('koa-route')
 
-app.use(_.get('/weather', function * () {
-  this.body = `Hello, your IP is ${this.request.ip}`
+// Replace native Promises with more performant + functional bluebird
+const Promise = require('bluebird')
+
+// node-fetch
+const fetch = require('node-fetch')
+fetch.Promise = Promise
+
+// Load configuration
+const config = require('./config.json')
+
+// query string manipulation
+const queryString = require('query-string')
+
+function getForecast (opts) {
+  opts = Object.assign(opts, { appid: config.openWeatherMap.apiKey })
+  let endpoint = `http://api.openweathermap.org/data/2.5/forecast?${queryString.stringify(opts)}`
+  return fetch(endpoint).then((res) => res.json())
+}
+
+app.use(_.get('/coords/:lat/:lon', function * (lat, lon) {
+  yield getForecast({lat, lon}).then((forecast) => {
+    this.body = forecast
+  })
 }))
 
 app.listen(3000)
